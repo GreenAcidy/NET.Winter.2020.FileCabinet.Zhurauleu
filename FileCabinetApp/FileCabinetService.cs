9cff3816518700daab53886f579a17604a108662
Kiryl Zhurauleu <kiryl.zhurauleu@gmail.com>
2020-02-02 21:16:58 +0300

    Add implementation for list command.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -1,4 +1,4 @@
-using System;
+п»їusing System;
 using System.Collections.Generic;
 
 namespace FileCabinetApp
@@ -24,8 +24,7 @@ namespace FileCabinetApp
 
         public FileCabinetRecord[] GetRecords()
         {
-            // TODO: добавьте реализацию метода
-            return Array.Empty<FileCabinetRecord>();
+            return this.list.ToArray();
         }
 
         public int GetStat()
